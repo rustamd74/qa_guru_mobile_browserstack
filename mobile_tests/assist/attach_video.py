3f0997f74227b30c580b74879243f99e1f84dd58
Rustam Dzhafarov <rustamd74@gmail.com>
2023-03-14 18:41:10 +0300

    Homework 20

--- mobile_tests/assist/attach_video.py
+++ mobile_tests/assist/attach_video.py
@@ -4,7 +4,7 @@ from allure_commons.types import AttachmentType
 
 
 def add_video(browser):
-    video_url = f"https://app-automate.browserstack.com/s3-upload/bs-video-logs-aps/s3.ap-west-1/" \
+    video_url = f"https://app-automate.browserstack.com/s3-upload/bs-video-logs-euw/s3.ap-west-1/" \
                 f"{browser.driver.session_id}/video-{browser.driver.session_id}.mp4"
     html = (
         "<html><body><video width='100%' height='100%' controls autoplay><source src='"
